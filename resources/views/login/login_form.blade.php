0752951f5de561b723fbb08f2aefa5479546739e
mkterer <>
2021-08-21 15:13:40 +0900

    npm run まで完了

---
+++ resources/views/login/login_form.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ログイン画面</title>
+    {{-- script --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- style --}}
+    <link rel="stylesheet" href="{{ asset('css/app.') }}">
+</head>
+<body>
+    
+</body>
+</html>
